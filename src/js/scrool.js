db2efb168a8597639880244b056387f639c1ee31
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2024-03-09 19:24:26 -0300

    content-below

---
+++ src/js/scrool.js
@@ -0,0 +1,13 @@
+const Observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach((element) => Observer.observe(element))
