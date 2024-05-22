f19702cb9ccc11a14ec0753e0f20913cd4882bb6
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2024-05-22 18:28:03 -0300

    movement only left right

--- src/js/gameRPG.js
+++ src/js/gameRPG.js
@@ -4,10 +4,9 @@ const canvas = document.querySelector('#canvas')
 canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 ctx = canvas.getContext('2d');
-let height = (window.innerHeight*50)/100
-let width = (window.innerWidth*50)/100
 
-ctx.fillRect(height,600,window.innerWidth-100,innerHeight-100)
+
+ctx.fillRect(50, 50, window.innerWidth-100, window.innerHeight-100)
 
 
 
@@ -16,9 +15,7 @@ ctx.fillRect(height,600,window.innerWidth-100,innerHeight-100)
 //movement character
 
 var dx; //Direction x
-var dy; //Direction Y
 var px; //position x
-var py; //position y
 var vel; //speed
 var obj; // Which obj will move
 var tmp; //timer
@@ -39,48 +36,31 @@ function keyDown() {
     var tecla = event.key; // GetArrowNumber
     if (tecla== "ArrowLeft" ) { //Left
         dx=-1;       
-    } else if (tecla == "ArrowUp") { //Up
-        dy=-1;
-        
     } else if (tecla == "ArrowRight") { //right
         dx=1;
         
-    } else if (tecla == "ArrowDown") { //bottom
-        dy=1;    
     }
 }
 function keyUp() {
     var tecla = event.key; 
     if (tecla=="ArrowLeft" ) { 
         dx=0;
-    } else if (tecla =="ArrowUp" ) { 
-        dy=0;
-    } else if (tecla == "ArrowRight" ) { 
+    }  else if (tecla == "ArrowRight" ) { 
         dx=0;
-    } else if (tecla == "ArrowDown" ) { 
-        dy=0;
-    }
+    } 
 }
 
 //px = posição X,
 function enterFrame() {
     var nextPx = px + dx * vel; //positionX + directionX * Speed
-    var nextPy = py + dy * vel;
+   
     if (nextPx < 0) {
         nextPx = 0;
     } else if (nextPx + obj.offsetWidth > window.innerWidth) {
         nextPx = window.innerWidth - obj.offsetWidth;
     }
 
-    if (nextPy < 0) {
-        nextPy = 0;
-    } else if (nextPy + obj.offsetWidth > window.innerHeight) { // can't be bigger than PC screen
-        nextPy = window.innerHeight - obj.offsetHeight;
-    }
-
     px = nextPx;
-    py = nextPy;
-   
     obj.style.left=px+'px'
     obj.style.top=py+'px'
 
