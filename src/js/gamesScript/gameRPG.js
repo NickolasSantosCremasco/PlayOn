b747ef8488840a9cfb46891ce8da767cc39cb703
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2024-07-22 20:55:15 -0300

    put more furniture

--- src/js/gamesScript/gameRPG.js
+++ src/js/gamesScript/gameRPG.js
@@ -47,7 +47,6 @@ function stopMovement() {
     currentFrame = 0
 }
 
-
 ctx.fillRect(50, 50, window.innerWidth-140, window.innerHeight-100);
 
 //movimento do personagem
@@ -59,20 +58,62 @@ var vel; //speed
 var obj; // Which obj will move
 var tmp; //timer
 
-obj=document.querySelector('#dv1')
+obj = document.querySelector('#dv1')
 obj.style.backgroundImage ='url("../../img/assets/AssetsPersonagemJogo/ParadoFrente.jpg")';
 
 // OBJETOS INTERAGÍVEIS
 //Lixo 
+const lixoPos = {x:window.innerWidth-500, y:window.innerHeight-500}
+const lixoSize = {width:100, height: 100}
+//Cama
+const camaPos = {x:window.innerWidth-1350, y:window.innerHeight-600}
+const camaSize = {width:80, height:130}
+// tapete
+const tapetePos = {x:window.innerWidth-1440, y:window.innerHeight-650}
+const tapeteSize = {width:250, height:250}
+//porta
+const portaPos = {x:window.innerWidth-900, y:window.innerHeight-250}
+const portaSize = {width:240, height:240}
+let currentObject = null
+
+function drawPorta () {
+    const portaImage = new Image();
+    portaImage.src = '../../img/assets/AssetsObjetosJogo/porta.png'
+    portaImage.onload = () => {
+        ctx.drawImage(portaImage, portaPos.x, portaPos.y, portaSize.width, portaSize.height);
+    }
+}
+
 function drawLixo() {
-    const lixoPos = {x:900, y:300}
-    const lixoSize = {width:100, height: 100}
     const lixoImage = new Image();
     lixoImage.src = '../../img/assets/AssetsObjetosJogo/lixo.png'
     lixoImage.onload = () => {
         ctx.drawImage(lixoImage,lixoPos.x, lixoPos.y, lixoSize.width, lixoSize.height )
-    }
+    };
+}
+function drawCama() {
+    const camaImage = new Image();
+    camaImage.src = '../../img/assets/AssetsObjetosJogo/CamaDesarrumada.jpg'
+    camaImage.onload = () => {
+        ctx.drawImage(camaImage, camaPos.x, camaPos.y, camaSize.width, camaSize.height)
+    };
 } 
+
+function drawTapete () {
+    const tapeteImage = new Image();
+    tapeteImage.src = '../../img/assets/AssetsObjetosJogo/Tapete.png'
+    tapeteImage.onload = () => {
+        ctx.drawImage(tapeteImage, tapetePos.x, tapetePos.y, tapeteSize.width, tapeteSize.height)
+    };
+}
+
+function drawObjects() {
+    drawTapete();
+    drawLixo();
+    drawCama();
+    drawPorta();
+   
+}
 
 
 function begin() {
@@ -126,6 +167,9 @@ function keyUp() {
     }
 }
 
+//  barra de loading
+let loadingInterval = null;
+let loadingProgress = 0
 
 //atualiza os frames do jogo, atualizações das posições e tamanho do mapa
 function enterFrame() {
@@ -147,8 +191,26 @@ function enterFrame() {
 
     py = nextPy
     px = nextPx;
+
+    //atualização do movimento do personagem na tela
     obj.style.left=px+'px'
-    obj.style.top=py+'px'   
+    obj.style.top=py+'px' 
+      
+    // se existir colisão com os objetos faça:
+    const collidedObject = checkColissionWithObjects(); // procura se tem colisão com objetos
+   if (collidedObject) {
+    if (currentObject !== collidedObject) { // se o objeto atual for diferente do objeto colidido
+        currentObject = collidedObject; // faça o objeto colidido virar o atual
+        loadingProgress = 0;
+    }
+    document.querySelector('.barra').style.display = 'block'   // barra de loading irá aparecer na tela
+    startLoading();
+    console.log(checkColissionWithObjects())
+   } else {
+   currentObject = null; 
+    stopLoading(); // se não houver colisão o loading para de ocorrer
+   }
+    // Verifica a proximidade com objetos
 }
 
 //Desenhando o mapa do jogo
@@ -156,7 +218,6 @@ function drawMap() {
     //Sprite do mapa
     const WoodSprite = new Image()
     WoodSprite.src = '../../img/assets/gameRPG/spriteChao.jpeg'
-    
     //Desenhando o jogo
     WoodSprite.onload = () => {
         //Desenhando o chão
@@ -166,7 +227,52 @@ function drawMap() {
             }
         }     
     }
-    drawLixo()
+    drawObjects()
+}
+
+// checar colisão com objetos do mapa
+function checkColissionWithObjects() {
+    if (px < lixoPos.x + lixoSize.width && px + obj.offsetWidth > lixoPos.x && py < lixoPos.y + lixoSize.height && py + obj.offsetHeight > lixoPos.y) { 
+        return 'lixo'
+    } else if (px < camaPos.x + camaSize.width && px + obj.offsetWidth > camaPos.x && py < camaPos.y + camaSize.height && py + obj.offsetHeight > camaPos.y) {
+        return 'cama'
+    }
+    return null
+}
+
+// começa o loading para remover o objeto da tela
+function startLoading() {
+    if (loadingInterval) return; // evitar múltiplas execuções enquanto uma ja estiver em andamento
+    loadingInterval = setInterval(() => {
+        loadingProgress += 10 // barra de progresso
+        document.querySelector('.barraCheia').style.width = `${loadingProgress}%`;
+            if (loadingProgress >= 100) {
+                clearInterval(loadingInterval)
+                //remover o objeto colidido
+                if (currentObject === 'lixo') {
+                    drawLixo = () => {} // o lixo desaparece
+                } else if (currentObject === 'cama') {
+                    drawCama = () => {
+                        const camaImage = new Image();
+                        camaImage.src = '../../img/assets/AssetsObjetosJogo/Cama.png'
+                        camaImage.onload = () => {
+                            ctx.drawImage(camaImage, camaPos.x, camaPos.y, camaSize.width, camaSize.height)
+                        };
+                    } // a Cama desaparece
+                }
+                drawMap() // redesenha o mapa acima do lixo desaparecido
+            }
+        }, 200
+    );
+}
+
+// o loading reseta
+function stopLoading () {
+    clearInterval(loadingInterval);
+    loadingInterval = null;
+    loadingProgress = 0;
+    document.querySelector('.barraCheia').style.width = '0%'
+    document.querySelector('.barra').style.display = 'none';
 }
 
 //Quando a tela for carregada o jogo se inicia
